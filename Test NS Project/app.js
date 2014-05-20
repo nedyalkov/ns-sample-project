d306b1e6a5371a3189243f5d0b59c10534824bab
Miroslav Nedyalkov <miroslav.nedyalkov@telerik.local>
2014-05-20 16:03:55 +0300

    Workspace created.

---
+++ Test NS Project/app.js
@@ -0,0 +1,13 @@
+var bclModule = require("bclmodule");
+
+var MainActivity = telerik.kimera.KimeraActivity.extends({
+   onCreate: function() {
+   }
+});
+
+app.init({
+    getActivity: function(intent) {
+    },
+    onCreate: function() {
+    } 
+});
